2a9b069f9a2628adad1fbcf3157a63d673fd6b3f
sunny <bug099999@gmail.com>
2021-11-15 10:59:12 +0800

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/taiwanTravel/'
-  : './',
+  publicPath: './',
   configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
